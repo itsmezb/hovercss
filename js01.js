758577d4e71db1edfd8853329dad4119d1fa123e
Kiyoo <zaidahsd9991@gmail.com>
2020-12-16 00:58:10 +0100

    Version 1

---
+++ js01.js
@@ -0,0 +1,27 @@
+
+// function rotateImage(matrix)  {
+//   let N = matrix.length;
+
+//   for (let i=0; i<N;i++) {
+//     for (let j=i; j<N; j++) {
+//       let temp = matrix[i][j];
+//       matrix[i][j] = matrix[j][i];
+//       matrix[j][i] = temp;
+//     }
+//   }
+//   for (let i=0;i<N;i++) {
+//     for (let j=0; j<(N/2);j++) {
+//       let temp = matrix[i][j];
+//       matrix[i][j] = matrix[i][N-1-j];
+//       matrix[i][N-1-j] = temp;
+//     }
+//   }
+//   return matrix;
+// }
+
+// const myImage = 
+// [[1,2,3],
+// [4,5,6,],
+// [7,8,9]];
+
+// console.log(rotateImage(myImage))
---
+++ js02.js
@@ -0,0 +1,4 @@
+//   function getCount(str) {
+//     return (str.match(/[aeiou]/ig)||[]).length;
+//   }
+// console.log(getCount("Can You Se dadme?"))
